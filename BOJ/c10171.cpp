d4189b15078cf2b99b927e10aae92f3605f92087
getCurrentThread <wak8835@gmail.com>
2020-02-03 12:41:51 +0900

    10171

---
+++ BOJ/c10171.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "\\    /\\\n )  ( ')\n(  /  )\n \\(__)|" << endl;
+	/*
+	printf(R"(\    /\
+ )  ( ')
+(  /  )
+ \(__)|)");
+	*/
+	return 0;
+}
