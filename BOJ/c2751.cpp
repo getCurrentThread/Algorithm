add3710185c042271aa6a2cb18fadbfd2b3fa548
getCurrentThread <wak8835@gmail.com>
2020-02-03 19:07:57 +0900

    2751-failed

---
+++ BOJ/c2751.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+#define MAX 1000001
+#define swap(a,b) (temp) = (a); (a) = (b); (b) = (temp);
+
+int arr[MAX];
+
+int partition(int list[], int left, int right)
+{
+	int temp;
+	int high, low, pivot;
+	
+	pivot = list[right];
+	low = left; 
+	high = right;
+
+	do {
+		for (; low <= high && list[low] < pivot; low++);
+		for (; low <= high && list[high] > pivot; high--);
+
+		if (low < high) {
+			swap(list[low], list[high]);
+		}
+
+	} while (low < high);
+
+	swap(list[left], list[high]); //low ,high가 교체한 위치에 피벗 넣기.
+
+	return high; //피벗 위치
+}
+
+void quickSort(int list[], int left, int right)
+{
+	if (!(left < right)) return;
+	
+	int q = partition(list, left, right); //피벗
+
+	quickSort(list, left, q - 1); //피벗보다 작은 왼쪽 리스트 정복
+	quickSort(list, q + 1, right);//피벗보다 큰 오른쪽 리스트 정복
+}
+
+int main() {
+	int num;
+
+	scanf("%d", &num);
+
+	for (int i = 0; i < num; i++) {
+		scanf("%d", &arr[i]);
+	}
+
+	quickSort(arr, 0, num-1);
+
+	for(int i = 0; i < num; i++){
+		printf("%d\n", arr[i]);
+	}
+
+	return 0;
+}
