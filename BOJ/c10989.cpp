32933e14912b1e2264f42731a9fb38aefd1e915b
getCurrentThread <wak8835@gmail.com>
2020-02-09 18:10:11 +0900

    10989 success. countingSort.

--- BOJ/c10989.cpp
+++ BOJ/c10989.cpp
@@ -1,66 +1,29 @@
 #include <iostream>
 
-#define swap(a,b) (temp) = (a); (a) = (b); (b) = (temp); 
-
-#define MAX 10000001
+#define MAX 10000
 
 using namespace std;
 
-int arr[MAX];
-
-int partition(int list[], int left, int right) {
-	int temp;
-	int low, high, mid, pivot;
-
-	mid = left + ((right - left) / 2);
-	low = left+1; high = right;
-
-	swap(list[left], list[mid]);
-
-	pivot = list[left];
-
-	while (low < high)
-	{
-		for (; low <= high && list[low] < pivot; low++);
-		for (; low <= high && list[high] > pivot; high--);
-
-		if (low < high){
-			swap(list[low], list[high]);
-		}
-	}
-
-	swap(list[left], list[high]);
-	
-	return high; //pivot 위치 반환.
-}
-
-void quickSort(int list[], int left, int right) {
-
-	if (left >= right)
-		return;
-
-	int p = partition(list, left, right);
-
-	quickSort(list, left, p - 1);
-	quickSort(list, p + 1, right);
-}
-
+int arr[MAX + 1] = { 0, };
 
 int main() {
 	int num;
+	int temp;
 	
 
 	scanf("%d", &num);
 
 
 	for (int i = 0; i < num; i++) {
-		scanf("%d", &arr[i]);
+		scanf("%d", &temp);
+		arr[temp]++;
 	}
 
-	quickSort(arr, 0, num-1);
-
-	for (int i = 0; i < num; i++) {
-		printf("%d\n", arr[i]);
+	for (int i = 0; i < MAX; i++) {
+		while(arr[i]){
+			printf("%d\n", i);
+			arr[i]--;
+		}
 	}
 
 	return 0;
