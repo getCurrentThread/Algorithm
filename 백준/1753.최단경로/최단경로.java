d07a1c34651d07316c86e004ae57466656a807ec
jinhyeok hyeon <31976959+getCurrentThread@users.noreply.github.com>
2022-01-09 19:29:18 +0900

    전체 코드 업로드

---
+++ 백준/1753.최단경로/최단경로.java
@@ -0,0 +1,86 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+import java.util.Vector;
+
+public class Main {
+	static int V, E;
+	static int start;
+	static boolean visited[];
+	static int dist[];
+	static List<List<int[]>> graph;
+	static final int INF = 987654321;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine());
+
+		V = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
+		start = Integer.parseInt(in.readLine());
+
+		graph = new ArrayList<>(V + 1);
+		for (int i = 0; i <= V; i++) {
+			graph.add(new ArrayList<>());
+		}
+		int u, v, w;
+		for (int i = 0; i < E; i++) {
+			st = new StringTokenizer(in.readLine());
+			u = Integer.parseInt(st.nextToken());
+			v = Integer.parseInt(st.nextToken());
+			w = Integer.parseInt(st.nextToken());
+			graph.get(u).add(new int[] { v, w });
+		}
+
+		daijstra();
+
+		for (int i = 1; i <= V; i++) {
+			System.out.println(dist[i] == INF? "INF": dist[i]);
+		}
+	}
+
+	private static void daijstra() {
+		visited = new boolean[V + 1];
+		dist = new int[V + 1];
+		Arrays.fill(dist, INF);
+		dist[start] = 0;
+
+		Comparator<int[]> comp = new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				return o1[1] - o2[1];
+			}
+		};
+		PriorityQueue<int[]> pq = new PriorityQueue<>(comp);
+		pq.add(new int[] { start, 0 });
+
+		int cnt = 0;
+		while (!pq.isEmpty()) {
+			int[] ele = pq.poll();
+			int v = ele[0];
+			int min = ele[1];
+			if (!visited[v]) {
+				visited[v] = true;
+
+				for (int i = graph.get(v).size() - 1; i >= 0; i--) {
+					int[] node = graph.get(v).get(i);
+					int u = node[0];
+					int weight = node[1];
+					if (!visited[u] && dist[u] > min + weight) {
+						dist[u] = min + weight;
+						pq.add(new int[] { u, dist[u] });
+					}
+				}
+
+				if (++cnt >= V)
+					return; // 모든 정점 순회 완료
+			}
+		}
+
+	}
+}
---
+++ 백준/2578.빙고/빙고.java
@@ -0,0 +1,67 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+	static int[][] board;
+	static final int N = 5;
+	static int bingoCnt = 0;
+	public static void checkBingo(int i, int j) {
+		board[i][j] = -1;
+		int r;
+		for(r = 0; r < 5; r++) {
+			if(board[r][j] != -1) {
+				break;
+			}
+		}
+		if(r == 5) bingoCnt++;
+		for(r = 0; r < 5; r++) {
+			if(board[i][r] != -1) {
+				break;
+			}
+		}
+		if(r == 5) bingoCnt++;
+		if(i == j) { // \대각선 체크
+			for(r = 0; r <5; r++) {
+				if(board[r][j] != -1) {
+					break;
+				}
+			}
+			if( r== 5) bingoCnt++;
+		}
+		if(i == 5-j-1) { // /대각선 체크
+			for(r = 0; r < 5; r++) {
+				if(board[r][5-r-1] != -1) {
+					break;
+				}
+			}
+			if(r == 5) bingoCnt++;
+		}
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		board = new int[N][N];
+		for(int i = 0; i < 5; i++) {
+			for(int j = 0; j < 5; j++) {
+				board[i][j] =sc.nextInt();
+			}
+		}
+		
+		for(int k = 1; k <= 25; k++) {
+			int tmp = sc.nextInt();
+			outer:
+			for(int i = 0; i < 5; i++) {
+				for(int j = 0; j < 5;j ++) {
+					if(board[i][j] == tmp) {
+						checkBingo(i, j);
+						break outer;
+					}
+				}
+			}
+			if(bingoCnt >= 3) {
+				System.out.println(k);
+				break;
+			}
+		}
+	}
+}
