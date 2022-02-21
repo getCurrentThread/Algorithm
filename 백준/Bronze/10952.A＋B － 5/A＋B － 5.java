db4c4c8657c49f74976b57b776b0f36499b551ea
jinhyeok hyeon <wak8835@gmail.com>
2022-02-21 15:45:54 +0900

    전체 코드 업로드 -BaekjoonHub

--- 백준/Bronze/10952.A＋B － 5/A＋B － 5.java
+++ 백준/Bronze/10952.A＋B － 5/A＋B － 5.java
@@ -1,21 +1,18 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder out = new StringBuilder();
-
         StringTokenizer st = new StringTokenizer(in.readLine());
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
-
-        System.out.println(oper(a, b));
-    }
-
-    private static int oper(int a, int b) {
-        return (a + b) * (a - b);
+        while (a != 0 && b != 0) {
+            System.out.println(a + b);
+            st = new StringTokenizer(in.readLine());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+        }
     }
 }
