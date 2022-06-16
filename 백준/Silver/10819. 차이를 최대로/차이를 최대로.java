ddef9f15ec1437b26ec5976bd4eb4b8bc5e1edb7
jinhyeok hyeon <wak8835@gmail.com>
2022-06-16 17:01:54 +0900

    [Silver II] Title: 차이를 최대로, Time: 100 ms, Memory: 11792 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.java
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -14,49 +13,35 @@ public class Main {
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(arr);
 
-        int mx = 0;
-        while (next_permutation(arr)) {
-            int sum = 0;
-            for (int i = 0; i < n - 1; i++) {
-                sum += Math.abs(arr[i] - arr[i + 1]);
-            }
+        boolean[] visited = new boolean[n];
+        System.out.println(dfs(arr, visited, 0, 0, 0));
+        int ans = 0;
+    }
+
+    static int mx = 0;
+
+    static int dfs(int[] arr, boolean[] visited, int depth, int prev, int sum) {
+        if (depth == arr.length) {
             mx = Math.max(mx, sum);
+            return mx;
+        }
+        if (depth == 0) {
+            for (int i = 0; i < arr.length; i++) {
+                visited[i] = true;
+                dfs(arr, visited, 1, i, 0);
+                visited[i] = false;
+            }
+            return mx;
         }
 
-        System.out.println(mx);
-    }
+        for (int i = 0; i < arr.length; i++) {
+            if (visited[i]) continue;
+            visited[i] = true;
+            dfs(arr, visited, depth + 1, i, sum + Math.abs(arr[i] - arr[prev]));
+            visited[i] = false;
+        }
 
-    private static boolean next_permutation(int[] arr) {
-        int i = arr.length - 2;
-        while (i >= 0 && arr[i] >= arr[i + 1]) {
-            i--;
-        }
-        if (i < 0) {
-            return false;
-        }
-        int j = arr.length - 1;
-        while (arr[j] <= arr[i]) {
-            j--;
-        }
-        swap(arr, i, j);
-        reverse(arr, i + 1);
-        return true;
-    }
-
-    private static void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-
-    private static void reverse(int[] arr, int i) {
-        int j = arr.length - 1;
-        while (i < j) {
-            swap(arr, i, j);
-            i++;
-            j--;
-        }
+        return mx;
     }
 }
