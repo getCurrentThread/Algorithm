f14da137f93c15727219fb0a9755e47c137b0e48
getCurrentThread <wak8835@gmail.com>
2022-09-09 14:51:29 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.02 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,20 +1,19 @@
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
 class Solution {
     public String solution(String s) {
-        return convertJadenCase(s);
+        return Arrays.stream(s.split(" "))
+                .map(x -> convertJadenCase(x)) // make lowercase...
+                .collect(Collectors.joining(" "));
     }
 
     public String convertJadenCase(final String s) {
-        char[] arr = s.toLowerCase().toCharArray();
-
-        // replace first character "a~z" -> 'A~Z'
+        char[] arr = s.toLowerCase(Locale.ROOT).toCharArray();
         if (arr.length > 0 && arr[0] >= 'a' && arr[0] <= 'z')
             arr[0] = (char) (arr[0] - 'a' + 'A');
-
-        for (int i = arr.length - 1; i > 0; --i) {
-            if (arr[i - 1] == ' ' && arr[i] >= 'a' && arr[i] <= 'z') {
-                arr[i] += 'A' - 'a';
-            }
-        }
         return String.valueOf(arr);
     }
+    
 }
